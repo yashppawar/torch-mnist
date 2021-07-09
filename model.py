77c39de780f4186c587b83478346e41d208a1b2b
Yash Pawar <yashpawarp@gmail.com>
2021-07-09 22:21:36 +0530

    ⚡️ update model

--- model.py
+++ model.py
@@ -10,8 +10,6 @@ class NeuralNetwork(nn.Module):
         self.linear_relu_stack = nn.Sequential(
             nn.Linear(28*28, 512),
             nn.ReLU(),
-            nn.Linear(512, 512),
-            nn.ReLU(),
             nn.Linear(512, 10),
             nn.ReLU()
         )
